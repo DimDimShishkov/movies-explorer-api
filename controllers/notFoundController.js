55ff93ad90a14fd42b9d22a0c40696aea4b5ea8d
DimDimShishkov <99988422+DimDimShishkov@users.noreply.github.com>
2022-11-09 13:14:49 +0400

    branch: level-1

--- controllers/notFoundController.js
+++
@@ -1,5 +0,0 @@
-const NotFound = require('../errors/NotFound');
-
-module.exports.notFoundController = (req, res, next) => {
-  next(new NotFound('Запрашиваемый ресурс не найден'));
-};
--- routes/users.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { getUserInfo, updateUserInfo } = require('../controllers/users');
-const { patchUserCelebrate } = require('../utils/celebrate');
-
-router.get('/me', getUserInfo); // возвращает информацию о пользователе (email и имя)
-router.patch('/me', patchUserCelebrate, updateUserInfo); // обновляет информацию о пользователе (email и имя)
-
-module.exports = router;
